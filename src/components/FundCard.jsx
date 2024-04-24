f13b84c45f84f7648e4e31016329c4d46bce7ea9
MIRACLE AGHOLOR <93255903+solomonMiracle@users.noreply.github.com>
2024-04-24 12:35:14 -0700

    Update FundCard.jsx

--- src/components/FundCard.jsx
+++ src/components/FundCard.jsx
@@ -7,13 +7,12 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
   const remainingDays = daysLeft(deadline);
   
   return (
-    <div className="sm:w-[288px] w-full rounded-[2px] bg-[blue] cursor- m-4" onClick={handleClick}>
+    <div className="sm:w-[288px] w-full rounded-[2px] bg-[black] cursor- m-4" onClick={handleClick}>
       <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[2px]"/>
 
       <div className="flex flex-col p-4">
         <div className="flex flex-row items-center mb-[18px]">
           <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
-          <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[white]">HealthCare</p>
         </div>
 
         <div className="block">
