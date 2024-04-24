370b67393fae0d37af5d37b87da32b3e70182b58
MIRACLE AGHOLOR <93255903+solomonMiracle@users.noreply.github.com>
2024-04-24 12:58:05 -0700

    Update CreateCampaign.jsx

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -41,7 +41,7 @@ const CreateCampaign = () => {
   }
 
   return (
-    <div className="bg-[black] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+    <div className="bg-[darkblue] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
       {isLoading && <Loader />}
       <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[black]">
         <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start Fundraising</h1>
